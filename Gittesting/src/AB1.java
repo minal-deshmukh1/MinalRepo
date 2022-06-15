30257c0e0e3547c1aab04fe6419ed6855a56a94a
minal-deshmukh1 <107535960+minal-deshmukh1@users.noreply.github.com>
2022-06-15 13:25:51 +0530

    Update AB1.java

--- Gittesting/src/AB1.java
+++ Gittesting/src/AB1.java
@@ -5,6 +5,11 @@ public class AB1 {
 	{
 		System.out.println("m1 method");
 	}
+	
+	public void m2()
+	{
+		System.out.println("m2 method");
+	}
 public static void main(String[] args) {
 	System.out.println("hii");
 }
